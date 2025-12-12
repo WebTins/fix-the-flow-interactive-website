5b8d478f63a9208dd3594633e8c1eb918ae3acec
Tin Nguyen <powertinnguyen@gmail.com>
2025-12-12 11:37:46 +0100

    Gereed knop toegevoegd
    Gereed knop heb ik toegevoegd, interactief gemaakt en gestijld. Verder heb ik de variabelen allemaal in het Engels aangepast. #29 #32

--- interactie.js
+++ interactie.js
@@ -1,10 +1,17 @@
 // Selecteer de button om alle artikelen te kunnen verbergen
-let filterKnop = document.querySelector('.filter-knop')
+let filterButton = document.querySelector('.filter-knop')
 
 // Even checken of de juiste button geselecteerd wordt
 // console.log(filterknop)
 // Wacht totdat iemand erop klikt
-filterKnop.addEventListener('click', function() {
+filterButton.addEventListener('click', function() {
     // Verberg alle artikelen
     document.querySelector('.filter').classList.toggle('filter-visible')
+})
+
+
+let applyButton = document.querySelector('.apply-button')
+
+applyButton.addEventListener('click', function (){
+    document.querySelector('.filter').classList.remove('filter-visible')
 })
